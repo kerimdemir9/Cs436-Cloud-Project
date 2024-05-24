e9a7ef713f22421ec708a25492abdaa406d6f446
Kerim Demir <kerim.demir2002@hotmail.com>
2024-05-24 21:51:20 +0300

    update

--- locustStressTest/locustfile.py
+++ locustStressTest/locustfile.py
@@ -2,51 +2,37 @@ from locust import HttpUser, task, between
 
 
 class WebsiteUser(HttpUser):
-    host = "http://34.38.118.130:3000"
+
+    host = "http://34.107.248.67:8888"
+
+    wait_time = between(2, 7)  # Simulates a wait time between tasks of 1 to 5 seconds
+
+    @task(1)
+    def load_documentation(self):
+        self.client.get("/api-docs")
+
+    @task(1)  # This task is 3 times more likely to be picked
+    def load_customers(self):
+        self.client.get("/v1/customers/find_all")
 
     @task(3)  # This task is 3 times more likely to be picked
-    def load_customers(self):
-        self.client.get("/todos")
+    def trigger_function(self):
+            self.client.get("/v1/transactions/calculate_user_average_income/1")
+
+    @task(2)  # This task is 3 times more likely to be picked
+    def trigger_function(self):
+        self.client.get("/v1/transactions/find_all_by_receiver/1")
 
 
-    # @task(5)
-    # def post_transaction(self):
-    #     data = {
-    #             "todo": "Finalize event theme",
-    #             "priority": "LOW",
-    #             "status": "TO DO",
-    #             "category": "HOME",
-    #             "dueDate": "2021-02-22"
-    #
-    #     }
-    #     headers = {'content-type': 'application/json'}
-    #
-    #     # Sending the POST request to a specific endpoint
-    #     self.client.post("/todos", json=data, headers=headers)
+    @task(10)
+    def post_transaction(self):
+        # Data to send in the POST request (as JSON)
+        data = {
+            "amount": 1.0,
+            "senderId": 3,
+            "receiverId": 1
+        }
+        headers = {'content-type': 'application/json'}
 
-
-
-#     host = "http://130.211.24.150:8888"
-# 
-#     wait_time = between(1, 5)  # Simulates a wait time between tasks of 1 to 5 seconds
-# 
-#     @task
-#     def load_documentation(self):
-#         self.client.get("/api-docs")
-# 
-#     @task(3)  # This task is 3 times more likely to be picked
-#     def load_customers(self):
-#         self.client.get("/v1/customers/find_all")
-# 
-#     @task(10)
-#     def post_transaction(self):
-#         # Data to send in the POST request (as JSON)
-#         data = {
-#             "amount": 1.0,
-#             "senderId": 43,
-#             "receiverId": 1
-#         }
-#         headers = {'content-type': 'application/json'}
-# 
-#         # Sending the POST request to a specific endpoint
-#         self.client.post("/v1/transactions/save", json=data, headers=headers)
+        # Sending the POST request to a specific endpoint
+        self.client.post("/v1/transactions/save", json=data, headers=headers)
